7483a69419d616b60446f34b21ed1c3dc3f46328
HakkeRosen <73590054+HakkeRosen@users.noreply.github.com>
2020-10-28 14:27:07 +0100

    uu

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hejsan CodeSandbox</h1>
+      <h1>Hejsan tjosan CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
