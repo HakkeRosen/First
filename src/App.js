0517fd564b386b32dba286acc69cb77b4ff37c0f
HakkeRosen <73590054+HakkeRosen@users.noreply.github.com>
2020-11-10 14:55:54 +0100

    hegfy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hejsan tjosan CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Skriv något så får du något magiskt!</h2>
     </div>
   );
 }
